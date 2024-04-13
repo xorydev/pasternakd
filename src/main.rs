20decab8a4aa03b3f9e456c48883adbb3ffc1511
Xorycode <xorycode@shuriken.pm>
2024-04-13 14:31:21 +0300

    add debug logic
    idk whether this should be pushed to prod, maybe the Listening thing but not the client thing, future me to decide lol

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
   let listener = net::TcpListener::bind("127.0.0.1:8492")
     .expect("Failed to listen on port 8492");
 
+  println!("Listening on port 8492");
+
   for stream in listener.incoming() {
+    println!("Handling Client");
     handle_client();
   }
 
