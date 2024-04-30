0bcaca47f31599ead4843c702a732dab7524353d
Xorycode <xorycode@shuriken.pm>
2024-05-01 00:39:04 +0300

    fix: implement improved filter (first implemented on quickill)

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ fn get_block_devices() -> Vec<String> {
     if output.status.success() {
       output_str = String::from_utf8(output.stdout).unwrap();
       block_devices = output_str.lines()
-        .filter(|line| !line.starts_with('|') && !line.starts_with('├') && !line.starts_with('└') && !line.starts_with('-'))
+        .filter(|line| !line.trim().contains('|') && !line.trim().contains('│') && !line.trim().contains('├') && !line.trim().contains('└') && !line.trim().contains('-'))
         .map(|line| line.to_string())
         .collect();
     }
